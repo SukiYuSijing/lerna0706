e6804837833730d0da96a4d74c4a33982e5d7837
SukiYuSijing <15767301655@163.com>
2022-07-07 01:36:59 +0800

    bootstrap hoist and clean

--- lerna-demo2/packages/project-a/src/App.vue
+++ lerna-demo2/packages/project-a/src/App.vue
@@ -2,7 +2,7 @@
  * @Author: SukiYuSijing 15767301655@163.com
  * @Date: 2022-07-07 00:30:22
  * @LastEditors: SukiYuSijing 15767301655@163.com
- * @LastEditTime: 2022-07-07 01:18:02
+ * @LastEditTime: 2022-07-07 01:33:03
  * @FilePath: \lerna0706\lerna-demo2\packages\project-a\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
